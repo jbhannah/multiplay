028c64869a020f2f3bee07d154f8660efc788ed1
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-10-25 08:54:28 -0700

    Establish database connection on startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,18 @@
     windows_subsystem = "windows"
 )]
 
+use std::{fs::create_dir_all, sync::Mutex};
+
+use diesel::{prelude::*, Connection, SqliteConnection};
+use tauri::Manager;
+
+#[derive(Default)]
+struct Library {
+    db: Mutex<Option<SqliteConnection>>,
+}
+
+const LIBRARY_FILENAME: &str = "library.sqlite";
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,6 +23,19 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .manage(Library::default())
+        .setup(|app| {
+            let app_dir = app.path_resolver().app_dir().unwrap();
+            create_dir_all(app_dir.clone()).expect("could not create app data directory");
+
+            let library_path = app_dir.join(LIBRARY_FILENAME);
+            *(app.state::<Library>()).db.lock().unwrap() = Some(
+                SqliteConnection::establish(library_path.to_str().unwrap())
+                    .expect("could not open library database"),
+            );
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/app.tsx
+++ src/app.tsx
@@ -3,25 +3,27 @@ import preactLogo from "./assets/preact.svg";
 import { Greeter } from "./components/greeter";
 import { SettingsProvider } from "./contexts/settings";
 
-export const App = () => (
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://preactjs.com" target="_blank">
-        <img src={preactLogo} class="logo preact" alt="Preact logo" />
-      </a>
+export const App = () => {
+  return (
+    <div class="container">
+      <h1>Welcome to Tauri!</h1>
+      <div class="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and Preact logos to learn more.</p>
+
+      <SettingsProvider>
+        <Greeter />
+      </SettingsProvider>
     </div>
-
-    <p>Click on the Tauri, Vite, and Preact logos to learn more.</p>
-
-    <SettingsProvider>
-      <Greeter />
-    </SettingsProvider>
-  </div>
-);
+  );
+};
